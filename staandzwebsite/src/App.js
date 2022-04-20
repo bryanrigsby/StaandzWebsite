27d263f57afdd4e22f0d322aa3f13c94317e06a2
Bryan Rigsby <bryanrigsby@att.net>
2022-04-20 10:16:20 -0500

    Update App.js

--- staandzwebsite/src/App.js
+++ staandzwebsite/src/App.js
@@ -2,7 +2,6 @@ import React, {useState, useEffect} from 'react'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import Image from 'react-bootstrap/Image'
 import { FcAndroidOs } from 'react-icons/fc';
 import { AiOutlineApple } from 'react-icons/ai';
 
