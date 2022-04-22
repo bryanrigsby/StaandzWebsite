11bdf7c703a8817830885c1085dfb5b97f76e00f
Bryan Rigsby <bryanrigsby@att.net>
2022-04-21 19:33:27 -0500

    added screenshots and adjusted site for desktop

--- staandzwebsite/src/App.js
+++ staandzwebsite/src/App.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Image from 'react-bootstrap/Image'
 import { FcAndroidOs } from 'react-icons/fc';
 import { AiOutlineApple } from 'react-icons/ai';
 
@@ -47,39 +48,71 @@ function App() {
   
   return (
       <Container className={'pb-5'} fluid style={{backgroundColor: '#89CFF0'}}>
+
+      
       <Row>
         <Col className={'text-center pt-2 pb-2'} style={{fontWeight: 'bold', fontSize: '2em', color: 'aliceblue', textShadow: '0px 2px 4px #282828', borderBottom: '2px solid darkgreen'}}>
           STAANDZ
         </Col>
       </Row>
 
-      <Row className={'mt-3'}>
+      <Row className={'d-none d-md-flex text-center mt-5'}>
+        <Col className={'text-center'} style={{fontWeight: 'bold', fontSize: '1.75em', color: 'aliceblue', textShadow: '0px 2px 4px #282828'}}>
+          Love mobile businesses like <span style={{color: 'darkgreen', textShadow: 'none', fontWeight: 'bolder'}}>FRUIT STANDS</span> and <span style={{color: 'darkgreen', textShadow: 'none', fontWeight: 'bolder'}}>FOOD TRUCKS</span> but have trouble finding them?</Col>
+      </Row>
+
+      <Row className={'mt-3 d-block d-md-none'}>
         <Col className={'text-center px-5'} style={{fontWeight: 'bold', fontSize: '1.25em', color: 'aliceblue', textShadow: '0px 2px 4px #282828'}}>
           Love mobile businesses like 
         </Col>
       </Row>
 
-      <Row className={'mt-3'}>
+      <Row className={'mt-3 d-block d-md-none'}>
         <Col className={'text-center'} style={{fontSize: '1.5em', color: 'darkgreen', fontWeight: 'bold'}}>
         <span style={{color: 'darkgreen', textShadow: 'none', fontWeight: 'bolder'}}>FRUIT STANDS</span>
         </Col>
       </Row>
 
-      <Row className={'mt-3'}>
+      <Row className={'mt-3 d-block d-md-none'}>
         <Col className={'text-center px-5'} style={{fontWeight: 'bold', fontSize: '1.25em', color: 'aliceblue', textShadow: '0px 2px 4px #282828'}}>
           and
         </Col>
       </Row>
 
-      <Row className={'mt-3'}>
+      <Row className={'mt-3 d-block d-md-none'}>
         <Col className={'text-center'} style={{fontSize: '1.5em', color: 'darkgreen', fontWeight: 'bold'}}>
         <span style={{color: 'darkgreen', textShadow: 'none', fontWeight: 'bolder'}}>FOOD TRUCKS</span>
         </Col>
       </Row>
       
-      <Row className={'mt-3'}>
+      <Row className={'mt-3 d-block d-md-none'}>
         <Col className={'text-center'} style={{fontWeight: 'bold', fontSize: '1.25em', color: 'aliceblue', textShadow: '0px 2px 4px #282828'}}>
         but have trouble finding them?
+        </Col>
+      </Row>
+
+
+      <Row className={'d-none d-md-flex text-center mt-5'}>
+        <Col sm={4}>
+          <Image  style={{border: '2px solid white'}} width={'300px'} src="images/homescreen.png"/>
+        </Col>
+        <Col sm={4}>
+          <Image  style={{border: '2px solid white'}} width={'300px'} src="images/map.png"/>
+        </Col>
+        <Col sm={4}>
+          <Image  style={{border: '2px solid white'}} width={'300px'} src="images/owner.png"/>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col className='text-center mb-4 mt-4 d-block d-md-none'>
+        <Image  style={{border: '2px solid white'}} width={'350px'} src="images/homescreen.png"/>
+        </Col>
+      </Row>
+
+      <Row className={'pb-md-4'} style={{borderBottom: '2px solid darkgreen'}}>
+        <Col className='text-center mb-4 d-block d-md-none'>
+        <Image  style={{border: '2px solid white'}} width={'350px'} src="images/map.png"/>
         </Col>
       </Row>
 
@@ -100,21 +133,32 @@ function App() {
         </Col>
       </Row>
 
-      <Row className={'mt-3'}>
+      <Row className={'d-none d-md-flex text-center mt-5'}>
+        <Col className={'text-center'} style={{fontWeight: 'bold', fontSize: '1.5em', color: 'aliceblue', textShadow: '0px 2px 4px #282828'}}>
+        Put your business on the map <span style={{color: 'darkgreen', textShadow: 'none', fontWeight: 'bolder'}}>LITERALLY!</span> Staandz customers will see your location in real time and can view your upcoming events and products with just a tap!</Col>
+      </Row>
+
+      <Row className={'mt-3 d-block d-md-none'}>
         <Col className={'text-center px-5'} style={{fontWeight: 'bold', fontSize: '1.25em', color: 'aliceblue', textShadow: '0px 2px 4px #282828'}}>
           Put your business on the map
         </Col>
       </Row>
 
-      <Row className={'mt-3'}>
+      <Row className={'mt-3 d-block d-md-none'}>
         <Col className={'text-center'} style={{fontSize: '1.5em', color: 'darkgreen', fontWeight: 'bold'}}>
         <span style={{color: 'darkgreen', textShadow: 'none', fontWeight: 'bolder'}}>LITERALLY!</span>
         </Col>
       </Row>
 
-      <Row className={'mt-3'}>
+      <Row className={'mt-3 mb-4 d-block d-md-none'}>
         <Col className={'text-center px-5'} style={{fontWeight: 'bold', fontSize: '1.25em', color: 'aliceblue', textShadow: '0px 2px 4px #282828'}}>
-          Staandz customers will see your location in real time and can view your upcoming events and products with just a tap
+          Staandz customers will see your location in real time and can view your upcoming events and products with just a tap!
+        </Col>
+      </Row>
+
+      <Row>
+        <Col className='text-center mb-4 d-block d-md-none'>
+        <Image  style={{border: '2px solid white'}} width={'350px'} src="images/owner.png"/>
         </Col>
       </Row>
 
